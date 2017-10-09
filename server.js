8023f3491bb312da0022f13e63ff27dd27a4223d
Amitay Soffer <amitaysoffer@gmail.com>
2017-10-09 13:55:32 +0300

    anguflix project first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var app = express();
+
+app.use(express.static('public'));
+app.use(express.static(__dirname + '/node_modules'));
+
+var port = 8000;
+app.listen(port, function(){
+    console.log('app listening on port ' + port);
+});
